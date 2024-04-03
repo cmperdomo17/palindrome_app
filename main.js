30e293618a0a756a3004bc9bfb91fa685f1dd205
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-04-02 23:40:00 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import './style.css'
+
+let close = document.getElementById('close-icon')
+let text = document.getElementById('textbox')
+let verify = document.getElementById('button-verify')
+let isPalCont = document.querySelector('.is-palindrome-container')
+let isntPalCont = document.querySelector('.isnt-palindrome-container')
+
+function isPalindrome(string) {
+    let array = string.split("");
+    let reverse = array.reverse().join("");
+    return string === reverse;
+}
+
+close.addEventListener('click', () => {
+    text.value = '';
+})
+
+verify.addEventListener('click', () => {
+    let string = text.value;
+    if (isPalindrome(string)) {
+        isPalCont.style.display = 'flex';
+        isntPalCont.style.display = 'none';
+    } else {
+        isntPalCont.style.display = 'flex';
+        isPalCont.style.display = 'none';
+    }
+})
